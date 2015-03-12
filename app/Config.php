50d2b159d90345b2a98fa30be22f7a5dfe7e7959
Vincent Loy <vincent.loy1@gmail.com>
2015-03-12 20:04:47 +0100

    ajout du fichier config/config.php et de la class Config.php

--- app/Config.php
+++ app/Config.php
@@ -11,4 +11,12 @@ namespace App;
 
 class Config {
 
+    private $settings = [];
+
+    public function __construct() {
+
+        $this->$settings = require dirname(__DIR__) . '/config/config.php';
+
+    }
+
 }
---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 12/03/2015
+ * Time: 19:59
+ */
+
+return array(
+    "db_name" => "base_blog",
+    "db_user" => "root",
+    "db_pass" => "",
+    "db_host" => "localhost"
+);
