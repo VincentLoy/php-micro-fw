b3673c610bf809902ae6c2ae9e80d3ded81614e9
Vincent Loy <vincent.loy1@gmail.com>
2015-03-14 16:19:28 +0100

    remove ugly pseudo cat security

--- pages/admin/categories/delete.php
+++ pages/admin/categories/delete.php
@@ -10,23 +10,7 @@ use Core\HTML\BootstrapForm;
 
 $categoriesTable = App::getInstance()->getTable('Category');
 
-if($_POST['id'] == 6) {
-    header("Location: admin.php?page=categories.index&msg=cat-error");
-}
-
 if(!empty($_POST)) {
-    $postTable = App::getInstance()->getTable('Post');
-
-
-    $posts = $postTable->lastByCategory($_POST['id']);
-
-    foreach($posts as $post) {
-        //var_dump($post);die;
-        $result = $postTable->update($post->id, [
-            'categorie_id' => 6,
-        ]);
-    }
-
     $result = $categoriesTable->delete($_POST['id']);
     header("Location: admin.php?page=categories.index");
 }
